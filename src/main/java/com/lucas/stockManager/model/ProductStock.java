5f3f0c0c93e00c84242c4301c04ead370dfd4b2c
LukasPio <lucaspio.galvao@gmail.com>
2024-07-20 16:03:50 -0300

    fix: optimize imports

--- src/main/java/com/lucas/stockManager/model/ProductStock.java
+++ src/main/java/com/lucas/stockManager/model/ProductStock.java
@@ -3,9 +3,6 @@ package com.lucas.stockManager.model;
 import com.lucas.stockManager.helpers.ProductStockId;
 import jakarta.persistence.*;
 
-import java.io.Serializable;
-import java.math.BigInteger;
-
 @Entity
 public class ProductStock {
     @EmbeddedId
