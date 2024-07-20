26c55dc873c25cbfa2c9f8030dabcdfdb743ac27
LukasPio <lucaspio.galvao@gmail.com>
2024-07-20 16:17:26 -0300

    feat: create product service

---
+++ src/main/java/com/lucas/stockManager/services/ProductService.java
@@ -0,0 +1,11 @@
+package com.lucas.stockManager.services;
+
+import com.lucas.stockManager.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+}
