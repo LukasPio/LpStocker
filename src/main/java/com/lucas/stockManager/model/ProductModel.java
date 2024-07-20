52fb158f7b0e5aae1b16b3fe6b25ca0ec94655fe
LukasPio <lucaspio.galvao@gmail.com>
2024-07-20 15:52:59 -0300

    feat: create category field, also add getters and setters

--- src/main/java/com/lucas/stockManager/model/ProductModel.java
+++ src/main/java/com/lucas/stockManager/model/ProductModel.java
@@ -1,12 +1,17 @@
 package com.lucas.stockManager.model;
 
+import com.lucas.stockManager.enums.ProductCategory;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
 @Entity
 @Table(name = "Product")
+@Getter
+@Setter
 public class ProductModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,5 +19,6 @@ public class ProductModel {
     private String name;
     private String description;
     private BigDecimal price;
+    private ProductCategory category;
 }
 
