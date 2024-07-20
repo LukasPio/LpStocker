6afbd52fa4c4c7d300e22f7128d52b1e5c5f1330
LukasPio <lucaspio.galvao@gmail.com>
2024-07-20 00:10:25 -0300

    initial commit

---
+++ src/main/java/com/lucas/stockManager/StockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.stockManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagerApplication.class, args);
+	}
+
+}
