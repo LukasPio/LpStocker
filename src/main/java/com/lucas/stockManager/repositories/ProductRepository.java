6585427346b83b3d954c2bd393e42f54cc786df2
LukasPio <lucaspio.galvao@gmail.com>
2024-07-20 16:17:41 -0300

    fix: add repository annotation

--- src/main/java/com/lucas/stockManager/repositories/ProductRepository.java
+++ src/main/java/com/lucas/stockManager/repositories/ProductRepository.java
@@ -2,8 +2,10 @@ package com.lucas.stockManager.repositories;
 
 import com.lucas.stockManager.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductModel, BigInteger> {
 }
--- src/main/java/com/lucas/stockManager/repositories/StockRepository.java
+++ src/main/java/com/lucas/stockManager/repositories/StockRepository.java
@@ -2,8 +2,10 @@ package com.lucas.stockManager.repositories;
 
 import com.lucas.stockManager.models.StockModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 
+@Repository
 public interface StockRepository extends JpaRepository<StockModel, BigInteger> {
 }
