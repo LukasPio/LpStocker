910d5345567f1480b5ed836925ae9398a1146629
LukasPio <lucaspio.galvao@gmail.com>
2024-07-21 15:55:24 -0300

    feat(productStockId): create no/all args constructors

--- src/main/java/com/lucas/stockManager/helpers/ProductStockId.java
+++ src/main/java/com/lucas/stockManager/helpers/ProductStockId.java
@@ -1,9 +1,14 @@
 package com.lucas.stockManager.helpers;
 
 import jakarta.persistence.Embeddable;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Embeddable
 public class ProductStockId implements Serializable  {
         private Long productId;
