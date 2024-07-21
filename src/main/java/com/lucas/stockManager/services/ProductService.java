1f1c7e419c060b14a74dbb7931bfaf6fd7902f32
LukasPio <lucaspio.galvao@gmail.com>
2024-07-21 15:49:38 -0300

    feat(productService): create get all products logic

--- src/main/java/com/lucas/stockManager/services/ProductService.java
+++ src/main/java/com/lucas/stockManager/services/ProductService.java
@@ -1,11 +1,26 @@
 package com.lucas.stockManager.services;
 
+import com.lucas.stockManager.dtos.ProductResponseDTO;
+import com.lucas.stockManager.models.ProductModel;
 import com.lucas.stockManager.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
+
+    public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
+        List<ProductModel> products = productRepository.findAll();
+        List<ProductResponseDTO> productResponseDTOS = new ArrayList<>();
+        for (ProductModel product : products) {
+            productResponseDTOS.add(new ProductResponseDTO(product));
+        }
+        return ResponseEntity.ok(productResponseDTOS);
+    }
 }
