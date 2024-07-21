a4bbef85e82893cb24b44616df2353bb90077f90
LukasPio <lucaspio.galvao@gmail.com>
2024-07-21 15:56:23 -0300

    feat(productStockId): create equals and hash code

--- src/main/java/com/lucas/stockManager/helpers/ProductStockId.java
+++ src/main/java/com/lucas/stockManager/helpers/ProductStockId.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Setter
 @Getter
@@ -15,4 +16,17 @@ import java.io.Serializable;
 public class ProductStockId implements Serializable  {
         private Long productId;
         private Long stockId;
+
+    @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                ProductStockId that = (ProductStockId) o;
+                return Objects.equals(productId, that.productId) && Objects.equals(stockId, that.stockId);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(productId, stockId);
+        }
 }
