679feca519569b9e7f27a93dedbc052b088aec2a
LukasPio <lucaspio.galvao@gmail.com>
2024-07-21 15:59:54 -0300

    feat(productController): create product controller with a get all endpoint

---
+++ src/main/java/com/lucas/stockManager/controllers/ProductController.java
@@ -0,0 +1,25 @@
+package com.lucas.stockManager.controllers;
+
+import com.lucas.stockManager.dtos.ProductResponseDTO;
+import com.lucas.stockManager.services.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(("/api/products"))
+public class ProductController {
+
+    private final ProductService productService;
+    ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getProducts() {
+        return productService.getAllProducts();
+    }
+}
