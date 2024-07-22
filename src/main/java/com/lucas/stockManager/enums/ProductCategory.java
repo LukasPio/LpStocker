7cfbd0f4fb73baa315be4a8a3bc4f195a334ade4
LukasPio <lucaspio.galvao@gmail.com>
2024-07-22 18:55:29 -0300

    refactor(productCategory): improving functionalities of product category enum

--- src/main/java/com/lucas/stockManager/enums/ProductCategory.java
+++ src/main/java/com/lucas/stockManager/enums/ProductCategory.java
@@ -1,9 +1,27 @@
 package com.lucas.stockManager.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ProductCategory {
-    ELECTRONICS,
-    BOOKS,
-    CLOTHING,
-    TOYS,
-    OTHER
+    ELECTRONICS("Electronics"),
+    BOOKS("Books"),
+    CLOTHING("Clothing"),
+    TOYS("Toys"),
+    OTHER("Other");
+
+    private final String category;
+
+    ProductCategory(String category) {
+        this.category = category;
+    }
+
+    public static boolean isValidCategory(String category) {
+        for (ProductCategory c : ProductCategory.values()) {
+            if (c.getCategory().equalsIgnoreCase(category)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
