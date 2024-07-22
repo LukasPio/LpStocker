859230353ba857470c666f81db0c850516f04985
LukasPio <lucaspio.galvao@gmail.com>
2024-07-22 17:58:48 -0300

    refactor(productResponseDTO): add id to product responses

--- src/main/java/com/lucas/stockManager/dtos/ProductResponseDTO.java
+++ src/main/java/com/lucas/stockManager/dtos/ProductResponseDTO.java
@@ -2,9 +2,16 @@ package com.lucas.stockManager.dtos;
 
 import com.lucas.stockManager.models.ProductModel;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
-public record ProductResponseDTO(String name, BigDecimal price, String description, String productCategory) {
+public record ProductResponseDTO(String name, BigDecimal price, String description, String category, BigInteger id) {
     public ProductResponseDTO(ProductModel productModel) {
-        this(productModel.getName(), productModel.getPrice(), productModel.getDescription(), String.valueOf(productModel.getCategory()));
+        this(
+                productModel.getName(),
+                productModel.getPrice(),
+                productModel.getDescription(),
+                String.valueOf(productModel.getCategory()),
+                productModel.getId()
+        );
     }
 }
