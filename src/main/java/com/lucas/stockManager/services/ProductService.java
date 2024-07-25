525a003bb74be69b1c6521b21adc53f34340e12f
LukasPio <lucaspio.galvao@gmail.com>
2024-07-24 22:20:44 -0300

    refactor(productService): adapt to new productRequestDTO validations

--- src/main/java/com/lucas/stockManager/services/ProductService.java
+++ src/main/java/com/lucas/stockManager/services/ProductService.java
@@ -2,14 +2,12 @@ package com.lucas.stockManager.services;
 
 import com.lucas.stockManager.dtos.ProductRequestDTO;
 import com.lucas.stockManager.dtos.ProductResponseDTO;
-import com.lucas.stockManager.enums.ProductCategory;
 import com.lucas.stockManager.models.ProductModel;
 import com.lucas.stockManager.repositories.ProductRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,35 +24,37 @@ public class ProductService {
         List<ProductModel> products = productRepository.findAll();
         if (products.isEmpty()) {
             return responseService.buildResponse(
-                    204,
+                    HttpStatus.NO_CONTENT.value(),
                     "There aren't any saved products in database.",
                     null
             );
         }
         List<ProductResponseDTO> productResponseDTOS = products.stream().map(ProductResponseDTO::new).toList();
         return responseService.buildResponse(
-                200,
+                HttpStatus.OK.value(),
                 "Get all products successfully",
                 productResponseDTOS
         );
     }
 
     public ResponseEntity<?> saveProduct(ProductRequestDTO productData) {
-        if (productData == null) return responseService.buildResponse(
+        if (productData.isAnyCampNull()) return responseService.buildResponse(
                     HttpStatus.BAD_REQUEST.value(),
                     "Any field of productRequest can be null.",
                     null);
 
-        if (!ProductCategory.isValidCategory(productData.category())) return responseService.buildResponse(
+        if (productData.isValidCategory()) return responseService.buildResponse(
                     HttpStatus.BAD_REQUEST.value(),
                     "The category entered is invalid.",
-                    null);
+                    null
+        );
 
         if (productRepository.existsByName(productData.name()))
             return responseService.buildResponse(
                     HttpStatus.CONFLICT.value(),
                     "Already exists a product with that name.",
-                    null);
+                    null
+            );
 
         ProductModel toSave = new ProductModel(productData);
         productRepository.save(toSave);
@@ -62,7 +62,7 @@ public class ProductService {
         return responseService.buildResponse(
                 HttpStatus.CREATED.value(),
                 "Product was saved succesfully.",
-                toSave
+                new ProductResponseDTO(toSave)
         );
     }
 }
