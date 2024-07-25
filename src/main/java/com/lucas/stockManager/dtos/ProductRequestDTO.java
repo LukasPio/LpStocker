f6dfe21564e845f73bfd5cdac462dc8ec91df34a
LukasPio <lucaspio.galvao@gmail.com>
2024-07-24 22:20:14 -0300

    refactor(productRequestDTO): add validations of ProductRequestDTO inside it

--- src/main/java/com/lucas/stockManager/dtos/ProductRequestDTO.java
+++ src/main/java/com/lucas/stockManager/dtos/ProductRequestDTO.java
@@ -1,6 +1,19 @@
 package com.lucas.stockManager.dtos;
 
+import com.lucas.stockManager.enums.ProductCategory;
+
 import java.math.BigDecimal;
 
 public record ProductRequestDTO(String name, BigDecimal price, String description, String category) {
+    public boolean isAnyCampNull() {
+        return name == null || price == null || description == null || category == null;
+    }
+    public boolean isValidCategory() {
+            for (ProductCategory aCategory : ProductCategory.values()) {
+                if (aCategory.getCategory().equalsIgnoreCase(this.category)) {
+                    return true;
+                }
+            }
+            return false;
+    }
 }
