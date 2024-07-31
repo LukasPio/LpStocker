3d68faff7ef68f8a6dc994fc5accfa0ded575ac2
LukasPio <lucaspio.galvao@gmail.com>
2024-07-30 22:45:53 -0300

    feat(stockService): create service to get all logic

---
+++ src/main/java/com/lucas/stockManager/services/StockService.java
@@ -0,0 +1,37 @@
+package com.lucas.stockManager.services;
+
+import com.lucas.stockManager.dtos.ProductResponseDTO;
+import com.lucas.stockManager.dtos.StockResponseDTO;
+import com.lucas.stockManager.models.StockModel;
+import com.lucas.stockManager.repositories.StockRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+    private final ResponseService responseService;
+    public StockService(StockRepository stockRepository, ResponseService responseService) {
+        this.stockRepository = stockRepository;
+        this.responseService = responseService;
+    }
+
+    public ResponseEntity<?> getAllStocks() {
+        List<StockModel> stocks = stockRepository.findAll();
+        if (stocks.isEmpty()) return responseService.buildResponse(
+                    HttpStatus.NOT_FOUND.value(),
+                    "There aren't saved stocks",
+                    ""
+            );
+        List<StockResponseDTO> productResponseDTOs = stocks.stream().map(StockResponseDTO::new).toList();
+        return responseService.buildResponse(
+                HttpStatus.OK.value(),
+                "Get all stocks successfully",
+                productResponseDTOs
+        );
+    }
+}
