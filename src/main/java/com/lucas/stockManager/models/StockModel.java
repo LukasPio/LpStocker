33b4d6943b8d53f994c76ad913378a2fdfcb3c94
LukasPio <lucaspio.galvao@gmail.com>
2024-07-30 23:07:05 -0300

    feat(stockModel): add column annotation to stock model attributes

--- src/main/java/com/lucas/stockManager/models/StockModel.java
+++ src/main/java/com/lucas/stockManager/models/StockModel.java
@@ -2,6 +2,7 @@ package com.lucas.stockManager.models;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigInteger;
@@ -10,11 +11,16 @@ import java.math.BigInteger;
 @Table(name = "stock")
 @Getter
 @Setter
+@NoArgsConstructor
 public class StockModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stock_id")
     private BigInteger id;
+    @Column(name = "stock_name")
     private String name;
+    @Column(name = "stock_totalcapacity")
     private BigInteger totalCapacity;
+    @Column(name = "stock_currentOccupation")
     private BigInteger currentOccupation;
 }
