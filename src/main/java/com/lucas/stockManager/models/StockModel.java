ad78f1fae3bce55212ff508d93961b278005ed75
LukasPio <lucaspio.galvao@gmail.com>
2024-08-01 22:41:45 -0300

    feat(stockModel): create constructor that receive stockRequestDTO

--- src/main/java/com/lucas/stockManager/models/StockModel.java
+++ src/main/java/com/lucas/stockManager/models/StockModel.java
@@ -1,5 +1,6 @@
 package com.lucas.stockManager.models;
 
+import com.lucas.stockManager.dtos.StockRequestDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,10 +18,19 @@ public class StockModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "stock_id")
     private BigInteger id;
+
     @Column(name = "stock_name")
     private String name;
+
     @Column(name = "stock_totalCapacity")
     private BigInteger totalCapacity;
-    @Column(name = "stock_currentOccupation")
+
+    @Column(name = "stock_currentOccupation", nullable = false)  // Ensure this matches the column name in the DB
     private BigInteger currentOccupation;
+
+    public StockModel(StockRequestDTO stockData) {
+        this.name = stockData.name();
+        this.totalCapacity = stockData.totalCapacity();
+        this.currentOccupation = stockData.currentOccupation();
+    }
 }
