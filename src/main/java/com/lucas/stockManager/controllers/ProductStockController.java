2db2540e9739e73bd1a6fd86a1ec4f835a17eacf
LukasPio <lucaspio.galvao@gmail.com>
2024-08-02 09:40:30 -0300

    feat(productStockController): create endpoints to get all productStocks

---
+++ src/main/java/com/lucas/stockManager/controllers/ProductStockController.java
@@ -0,0 +1,22 @@
+package com.lucas.stockManager.controllers;
+
+import com.lucas.stockManager.services.ProductStockService;
+import com.lucas.stockManager.services.StockService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/productStock")
+public class ProductStockController {
+    private final ProductStockService productStockService;
+    public ProductStockController(ProductStockService productStockService) {
+        this.productStockService = productStockService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllProductStock() {
+        return productStockService.getAllProductStocks();
+    }
+}
