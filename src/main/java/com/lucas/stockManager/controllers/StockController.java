1f26b66f8fcf0253c30ae1ab81485b0a7f44fcc4
LukasPio <lucaspio.galvao@gmail.com>
2024-08-01 22:42:12 -0300

    feat(stockController): create routes to get by id and save to stock

--- src/main/java/com/lucas/stockManager/controllers/StockController.java
+++ src/main/java/com/lucas/stockManager/controllers/StockController.java
@@ -1,20 +1,30 @@
 package com.lucas.stockManager.controllers;
 
+import com.lucas.stockManager.dtos.StockRequestDTO;
 import com.lucas.stockManager.services.StockService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
 
 @RestController
 @RequestMapping("/api/stocks")
 public class StockController {
     private final StockService stockService;
-    public StockController(StockService stockService) {
-        this.stockService = stockService;
-    }
+    public StockController(StockService stockService) { this.stockService = stockService; }
+
     @GetMapping
     public ResponseEntity<?> getAllStocks() {
         return stockService.getAllStocks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStockById(@PathVariable BigInteger id) {
+        return stockService.getStockById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveStock(@RequestBody StockRequestDTO stockData) {
+        return stockService.saveStock(stockData);
     }
 }
