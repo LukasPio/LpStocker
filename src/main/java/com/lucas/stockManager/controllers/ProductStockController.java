9333bc7494f989e90830bc26f97ded14f9ce1b64
LukasPio <lucaspio.galvao@gmail.com>
2024-08-02 09:42:24 -0300

    refactor(productStockController): optimize imports

--- src/main/java/com/lucas/stockManager/controllers/ProductStockController.java
+++ src/main/java/com/lucas/stockManager/controllers/ProductStockController.java
@@ -1,7 +1,6 @@
 package com.lucas.stockManager.controllers;
 
 import com.lucas.stockManager.services.ProductStockService;
-import com.lucas.stockManager.services.StockService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
