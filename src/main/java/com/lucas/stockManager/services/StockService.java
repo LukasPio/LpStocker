eb68ae1cb3e3367531e6b624153fafb4c559bf5e
LukasPio <lucaspio.galvao@gmail.com>
2024-08-01 22:38:57 -0300

    feat(stockService): create logic to save/get by id stocks

--- src/main/java/com/lucas/stockManager/services/StockService.java
+++ src/main/java/com/lucas/stockManager/services/StockService.java
@@ -1,5 +1,6 @@
 package com.lucas.stockManager.services;
 
+import com.lucas.stockManager.dtos.StockRequestDTO;
 import com.lucas.stockManager.dtos.StockResponseDTO;
 import com.lucas.stockManager.models.StockModel;
 import com.lucas.stockManager.repositories.StockRepository;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 import java.util.List;
 
 @Service
@@ -24,13 +26,42 @@ public class StockService {
         if (stocks.isEmpty()) return responseService.buildResponse(
                     HttpStatus.NO_CONTENT.value(),
                     "There aren't saved stocks",
-                    ""
+                    ' '
             );
         List<StockResponseDTO> stockResponseDTOs = stocks.stream().map(StockResponseDTO::new).toList();
         return responseService.buildResponse(
                 HttpStatus.OK.value(),
                 "Get all stocks successfully",
                 stockResponseDTOs
+        );
+    }
+
+    public ResponseEntity<?> getStockById(BigInteger id) {
+        StockModel stock = stockRepository.findById(id).orElse(null);
+        if (stock == null) return responseService.buildResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "There aren't any save stock with id: " + id,
+                ' '
+        );
+        return responseService.buildResponse(
+                HttpStatus.OK.value(),
+                "Got stock with id " + id + " successfully",
+                new StockResponseDTO(stock)
+        );
+    }
+
+    public ResponseEntity<?> saveStock(StockRequestDTO stockData) {
+        if (stockData.isAnyCampNull()) return responseService.buildResponse(
+                    HttpStatus.BAD_REQUEST.value(),
+                    "Any camp can be null",
+                    ' '
+            );
+        StockModel stock = new StockModel(stockData);
+        stockRepository.save(stock);
+        return responseService.buildResponse(
+                HttpStatus.OK.value(),
+                "Stock was saved successfully",
+                stockData
         );
     }
 }
